bcd5a56348a1b3b795c0f6b7bc897aff82eaff14
DrCBeatz <brynecarruthers@gmail.com>
2023-01-22 07:34:52 -0500

    removed port #

--- pdo.php
+++ pdo.php
@@ -4,8 +4,9 @@
 // echo "<pre>\n";
 
 // stores connection to database in object called $PDO
-$pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;port=8889;dbname=heroku_1c173b5d5bc86a2','b89a80ef248678','2a576ba3');
-// mysql://b89a80ef248678:2a576ba3@us-cdbr-east-06.cleardb.net/heroku_1c173b5d5bc86a2?reconnect=true
+$pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_1c173b5d5bc86a2','b89a80ef248678','zap');
+// $pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;port=8889;dbname=heroku_1c173b5d5bc86a2','b89a80ef248678','zap');
+mysql://b89a80ef248678:2a576ba3@us-cdbr-east-06.cleardb.net/heroku_1c173b5d5bc86a2?reconnect=true
 // Tuns on error messages in PDO
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

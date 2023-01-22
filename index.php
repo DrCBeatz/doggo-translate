75a3734b348c593a807ff574111fdd64f354b8b1
DrCBeatz <brynecarruthers@gmail.com>
2023-01-22 16:23:59 -0500

    changed db env variable name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     echo "Oh hai!";
     //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_url = parse_url(getenv("CLEARDB_GREEN_URL"));
     $cleardb_server = $cleardb_url["host"];
     $cleardb_username = $cleardb_url["user"];
     $cleardb_password = $cleardb_url["pass"];
